44d5e94f70c577b82780457666f7d52da42336f0
Emilio Rivers <rivers.emilio2001@protonmail.com>
2022-05-28 23:58:17 -0500

    Final Version v2

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-var http = require("http");
-var https = require("https");
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('sslcert/privatekey.pem', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+const path = require("path");
 
-var fs = require("fs");
-var privateKey = fs.readFileSync("sslcert/privatekey.pem", "utf8");
-var certificate = fs.readFileSync("sslcert/server.crt", "utf8");
 var credentials = {
-	key: privateKey,
-	cert: certificate
+    key: privateKey,
+    cert: certificate
+    //En caso de que protejan su llave agreguen el atributo passphrase: '<su frase>'
 };
-
-var express = require("express");
+var express = require('express');
 var app = express();
-app.use(express.static(__dirname));
 
-app.get("/", (req, res) => {
-	res.sendFile("public/index.html", {root: __dirname });
-});
+// your express configuration here
 
+app.get('/principal',(req,res)=>{
+    console.log("Entre");
+    res.sendFile(path.join(__dirname,'views','Page.html'))
+})
+
+//No es necesario que tengan tanto el protocolo http y https funcionando al mismo tiempo
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(8080, ()=> console.log("HTTP server running on PORT 8080"));
-httpsServer.listen(8443, ()=> console.log("HTTPS server running on PORT 8443"));
+httpServer.listen(8383,()=>console.log("Corriendo http 8383"));
+//comentar el de arriba
+httpsServer.listen(8080,()=>console.log("Corriendo HTTPS 8080"));
